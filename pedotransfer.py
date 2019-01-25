686a4ff6e327735b28dc6422bd6a799738971acb
Aakash Ahamed <ahamednasa@gmail.com>
2019-01-25 14:25:17 -0800

    add pedotransfer.py

---
+++ pedotransfer.py
@@ -0,0 +1,10 @@
+
+# get bbox
+
+# Feed into this url:
+
+url = "https://sdmdataaccess.nrcs.usda.gov/Spatial/SDMNAD83Geographic.wfs?Service=WFS&Version=1.0.0&Request=GetFeature&Typename=MapunitPoly&BBOX=-120.720117590223,40.6199100459223,-120.719882036466,40.6200899886595"
+
+# get the areacode
+
+# query that in the lookup table
