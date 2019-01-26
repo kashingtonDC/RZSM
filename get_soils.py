57f2bc8f69196d145796006226b34e27b5c18e75
kashingtondc <ahamednasa@gmail.com>
2019-01-25 16:53:19 -0800

    add soil texture scraper and textures.json

--- get_soils.py
+++ get_soils.py
@@ -6,6 +6,9 @@ import ee
 import xml.etree.ElementTree
 import requests
 import re
+from itertools import islice
+import json
+
 
 ee.Initialize()
 
@@ -24,26 +27,30 @@ data_dir = os.path.join(os.getcwd(),"data")
 txts = glob.glob(os.path.join(data_dir,"*.txt"))
 stations_csv = csvs[0]
 
-good_sites = 0
-bad_sites = 0
+# setup textures out dict
+textures = {}
 
+# Loop through site files
 for i in txts:
+	site_no = ''.join(c for c in i if c.isdigit())
+
+	print(site_no)
+
+	# Using the lat / longs, create a small buffer and bbox, submit this to NRCS / USDA query string to get the "areasymbol"
+	lat, lon = get_site_lat_lons(site_no,stations_csv)
+	pt = ee.Geometry.Point([lon, lat])
+	buffer_size = 10 # meters
+	area = pt.buffer(buffer_size)
+	bounds = area.bounds()
+
+	# Extract upper left and lower right coordinates for the query 
+	ul = bounds.getInfo()['coordinates'][0][0]
+	lr = bounds.getInfo()['coordinates'][0][2]
+
+	# query the webpage and get the map unit id
+	query_string = "https://sdmdataaccess.nrcs.usda.gov/Spatial/SDMNAD83Geographic.wfs?Service=WFS&Version=1.0.0&Request=GetFeature&Typename=MapunitPoly&BBOX={},{},{},{}".format(ul[0], ul[1],lr[0], lr[1])
+
 	try:
-		site_no = ''.join(c for c in i if c.isdigit())
-
-		# Using the lat / longs, create a small buffer and bbox, submit this to NRCS / USDA query string to get the "areasymbol"
-		lat, lon = get_site_lat_lons(site_no,stations_csv)
-		pt = ee.Geometry.Point([lon, lat])
-		buffer_size = 10 # meters
-		area = pt.buffer(buffer_size)
-		bounds = area.bounds()
-
-		# Extract upper left and lower right coordinates for the query 
-		ul = bounds.getInfo()['coordinates'][0][0]
-		lr = bounds.getInfo()['coordinates'][0][2]
-
-		# query the webpage and get the map unit id
-		query_string = "https://sdmdataaccess.nrcs.usda.gov/Spatial/SDMNAD83Geographic.wfs?Service=WFS&Version=1.0.0&Request=GetFeature&Typename=MapunitPoly&BBOX={},{},{},{}".format(ul[0], ul[1],lr[0], lr[1])
 
 		req = requests.request('GET', query_string)
 		xml = req.text
@@ -55,16 +62,24 @@ for i in txts:
 		# Now parse the sand/silt/clay db to get the fractions 
 		texture_file = [x for x in os.listdir(os.getcwd()) if x.endswith('ascii')][0]
 
+		textures[site_no] = []
+
+		# Search the lookup table for the soil mapunits, if present, get the textures and append to textures dict. 
 		with open(texture_file) as f:
 			for line in f:
 				if mapunit in line:
-					good_sites+= 1
-					print(line)
-				else:
-					bad_sites+=1
+					values = []
+					values.append(line)
+					values.append(''.join(islice(f, 2)))
+					flat = "".join(values)
+					out = [i for i in flat.split()]
+					textures[site_no] = out
+
 	except:
 		print(i + " FAILED") 
-		bad_sites+=1
 
-print(good_sites)
-print(bad_sites)
+print(textures)
+print(len(textures))
+
+with open('textures.json', 'w') as fp:
+    json.dump(textures, fp)
